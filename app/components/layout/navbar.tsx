8af387c926ee8608fc6f378dd080af4390aab748
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-08-03 16:23:09 +0200

    init

---
+++ app/components/layout/navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+
+    return (
+        <div className="bg-gray-50">
+            <div className="flex max-w-7xl mx-auto p-8">
+                <div className="flex-1">
+                    logo
+                </div>
+                <div>Login</div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
